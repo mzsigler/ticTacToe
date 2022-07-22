eb9d733cf115345f704b5e886e0d5273f9b50484
Mikel Sigler <mzsigler@gmail.com>
2022-07-22 12:39:23 -0500

    let me back this up before I break it

--- script.js
+++ script.js
@@ -1,9 +1,94 @@
 const boxes = document.querySelectorAll('.box');
-const boxArray = Array.from([length = 9]);
-console.log(boxArray)
+const boxArray = [
+    {
+        id: 1,
+        text: ""
+    },
 
-boxes.forEach(box => {
-    box.addEventListener("click", () => {
-        console.log(`I am ${box.id}`)
+    {
+        id: 2,
+        text: ""
+    },
+
+    {
+        id: 3,
+        text: ""
+    },
+
+    {
+        id: 4,
+        text: ""
+    },
+
+    {
+        id: 5,
+        text: ""
+    },
+
+    {
+        id: 6,
+        text: ""
+    },
+
+    {
+        id: 7,
+        text: ""
+    },
+
+    {
+        id: 8,
+        text: ""
+    },
+
+    {
+        id: 9,
+        text: ""
+    },
+
+];
+const makeBoxesButton = document.getElementById('makeBoxes');
+const gameBoard = document.querySelector('.gameBoard');
+
+const playerFactory = (name, symbol) => {
+    return {
+        name,
+        symbol,
+    }
+}
+
+//iife to load the game board
+
+(() => {
+    boxArray.forEach((box) => {
+        const gameboard = document.querySelector('.gameBoard');
+        const newBox = document.createElement('div');
+        newBox.classList.add('box');
+        newBox.setAttribute('id', `${box.id}`)
+        newBox.innerText = `${box.text}`
+        gameboard.appendChild(newBox);
     })
-})
+})();
+
+
+gameBoard.addEventListener('click', (e) => {
+    const index = parseInt(e.target.id);
+    console.log(index);
+    selectedBoxRef = boxArray.find(box => {
+        return box.id === index;
+    })
+    selectedBoxRef.text = "X";
+    updateBoxes();
+});
+
+function updateBoxes(){
+    const gameboard = document.querySelector('.gameBoard');
+    gameboard.innerHTML = '';
+    boxArray.forEach(box => {
+    const newBox = document.createElement('div');
+    newBox.classList.add('box');
+    newBox.setAttribute('id', `${box.id}`)
+    newBox.innerText = `${box.text}`;
+    gameboard.appendChild(newBox);
+    })
+};
+
